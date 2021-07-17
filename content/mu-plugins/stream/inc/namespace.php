9157f3f180fa46dee84504a2a00f1d464161a1e3
Ivan Kristianto <ivan.kristianto@gmail.com>
2021-07-17 19:05:04 +0700

    Add stream folder

---
+++ content/mu-plugins/stream/inc/namespace.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Stream Log
+ */
+
+namespace Skeleton\Stream;
+
+/**
+ * Bootstrapper
+ */
+function bootstrap() {
+	add_action( 'plugins_loaded', __NAMESPACE__ . '\\on_plugins_loaded', 1 );
+}
+
+/**
+ * Load plugins.
+ *
+ * @return void
+ */
+function on_plugins_loaded(): void {
+	if ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) {
+		return;
+	}
+
+	if ( ! defined( 'WPMU_PLUGIN_DIR' ) ) {
+		return;
+	}
+
+	add_filter( 'wp_stream_is_network_activated', '__return_true' );
+
+	require_once WPMU_PLUGIN_DIR . '/vendor/stream/stream.php';
+}
---
+++ content/mu-plugins/stream/load.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Stream Log
+ */
+
+namespace Skeleton\Stream;
+
+require_once __DIR__ . '/inc/namespace.php';
+
+// Kickstart
+bootstrap();
