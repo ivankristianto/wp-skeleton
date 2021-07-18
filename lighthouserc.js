f8cf6d4655e4398491b1c1b5d0fb4fd832234afb
Ivan Kristianto <ivan.kristianto@gmail.com>
2021-07-18 14:20:05 +0700

    Add Lighthouse CI

---
+++ lighthouserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	ci: {
+		upload: {
+			target: 'lhci',
+		},
+		assert: {
+			'budgetsFile': "./.config/budget.json",
+			preset: 'lighthouse:recommended',
+			'assertions': {
+				'dom-size': [ 'error', { 'maxNumericValue': 3000 } ],
+				'first-contentful-paint': [
+					'warn',
+					{ 'maxNumericValue': 3000 } ],
+				'viewport': 'error',
+				'installable-manifest': 'off',
+				'offline-start-url': 'off',
+				'service-worker': 'off',
+				'splash-screen': 'off',
+				'works-offline': 'off',
+			},
+		},
+	},
+};
